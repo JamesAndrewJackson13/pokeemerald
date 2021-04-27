d5346e4944e07647fbd2d29ed437b5744ca33854
GnollCanDo <james.andrew.jackson13@gmail.com>
2021-04-26 23:58:45 -0700

    minor fixes to the repel improvement code

--- src/script_menu.c
+++ src/script_menu.c
@@ -107,7 +107,7 @@ static void DrawMultichoiceMenuCustom(u8 left, u8 top, u8 multichoiceId, u8 igno
     SetStandardWindowBorderStyle(windowId, 0);
     PrintMenuTable(windowId, count, actions);
     InitMenuInUpperLeftCornerPlaySoundWhenAPressed(windowId, count, cursorPos);
-    schedule_bg_copy_tilemap_to_vram(0);
+    ScheduleBgCopyTilemapToVram(0);
     InitMultichoiceCheckWrap(ignoreBPress, count, windowId, multichoiceId);
 }
 
